e3186e9f1e4a949a90e73639fdced6091f4c681b
Anna Litvinova <litvinova4815@gmail.com>
2022-10-05 19:55:29 +0300

    Final commit

--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
@@ -14,8 +15,10 @@ class MobileBankApiTestV1 {
       // Выполняемые действия
       .when()
           .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+
+              .then()
+              .statusCode(200)
+
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
